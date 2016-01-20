251093b48a6c1e0b83ecce62914bc8f80b1e2089
Tomas <magyc@rambler.ru>
2016-01-20 17:34:24 +0200

    fix pg db settings

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20160118140614) do
 
-  create_table "coments", force: :cascade do |t|
-    t.string   "name"
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "coments", ["post_id"], name: "index_coments_on_post_id"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -31,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160118140614) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -55,7 +48,8 @@ ActiveRecord::Schema.define(version: 20160118140614) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "posts"
 end
